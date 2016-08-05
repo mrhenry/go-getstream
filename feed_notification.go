e533a1b7a0f2512297c340e20bb663fbda67c75c
romainmenke <romainmenke@gmail.com>
2016-08-05 22:17:02 +0200

    notification test

--- feed_notification.go
+++ feed_notification.go
@@ -220,10 +220,10 @@ func (f *NotificationFeed) UnfollowKeepingHistory(target *FlatFeed) error {
 
 }
 
-// FollowersWithLimitAndSkip returns a list of GeneralFeed following the current NotificationFeed
-func (f *NotificationFeed) FollowersWithLimitAndSkip(limit int, skip int) ([]*GeneralFeed, error) {
+// FollowingWithLimitAndSkip returns a list of GeneralFeed followed by the current FlatFeed
+func (f *NotificationFeed) FollowingWithLimitAndSkip(limit int, skip int) ([]*GeneralFeed, error) {
 
-	endpoint := "feed/" + f.FeedSlug + "/" + f.UserID + "/" + "followers" + "/"
+	endpoint := "feed/" + f.FeedSlug + "/" + f.UserID + "/" + "following" + "/"
 
 	payload, err := json.Marshal(&getNotificationFeedFollowersInput{
 		Limit: limit,
@@ -235,7 +235,7 @@ func (f *NotificationFeed) FollowersWithLimitAndSkip(limit int, skip int) ([]*Ge
 
 	resultBytes, err := f.get(endpoint, f.Signature(), payload)
 
-	output := &getNotificationFeedFollowersOutput{}
+	output := &getFlatFeedFollowersOutput{}
 	err = json.Unmarshal(resultBytes, output)
 	if err != nil {
 		return nil, err
@@ -252,7 +252,7 @@ func (f *NotificationFeed) FollowersWithLimitAndSkip(limit int, skip int) ([]*Ge
 		}
 
 		if match {
-			firstSplit := strings.Split(result.FeedID, ":")
+			firstSplit := strings.Split(result.TargetID, ":")
 
 			feed.FeedSlug = firstSplit[0]
 			feed.UserID = firstSplit[1]
---
+++ feed_notification_test.go
@@ -0,0 +1,291 @@
+package getstream
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleNotificationFeed_AddActivity() {
+
+	client, err := New("APIKey", "APISecret", "AppID", "Region")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	feed, err := client.NotificationFeed("FeedSlug", "UserID")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	activity, err := feed.AddActivity(&NotificationFeedActivity{
+		Verb:      "post",
+		ForeignID: "48d024fe-3752-467a-8489-23febd1dec4e",
+		Object:    FeedID("flat:eric"),
+		Actor:     FeedID("flat:john"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_ = activity
+}
+
+func TestNotificationFeedAddActivity(t *testing.T) {
+
+	client, err := testSetup()
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	feed, err := client.NotificationFeed("notification", "bob")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	activity, err := feed.AddActivity(&NotificationFeedActivity{
+		Verb:      "post",
+		ForeignID: "48d024fe-3752-467a-8489-23febd1dec4e",
+		Object:    FeedID("flat:eric"),
+		Actor:     FeedID("flat:john"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	if activity.Verb != "post" && activity.ForeignID != "48d024fe-3752-467a-8489-23febd1dec4e" {
+		t.Fail()
+	}
+
+	err = testCleanUp(client, nil, []*NotificationFeedActivity{activity})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+}
+
+func TestNotificationFeedRemoveActivity(t *testing.T) {
+
+	client, err := testSetup()
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	feed, err := client.NotificationFeed("notification", "bob")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	activity, err := feed.AddActivity(&NotificationFeedActivity{
+		Verb:   "post",
+		Object: FeedID("flat:eric"),
+		Actor:  FeedID("flat:john"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	if activity.Verb != "post" {
+		t.Fail()
+	}
+
+	rmActivity := NotificationFeedActivity{
+		ID: activity.ID,
+	}
+
+	err = feed.RemoveActivity(&rmActivity)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+}
+
+func TestNotificationFeedRemoveByForeignIDActivity(t *testing.T) {
+
+	client, err := testSetup()
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	feed, err := client.NotificationFeed("notification", "bob")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	activity, err := feed.AddActivity(&NotificationFeedActivity{
+		Verb:      "post",
+		ForeignID: "08f01c47-014f-11e4-aa8f-0cc47a024be0",
+		Object:    FeedID("flat:eric"),
+		Actor:     FeedID("flat:john"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	if activity.Verb != "post" && activity.ForeignID != "08f01c47-014f-11e4-aa8f-0cc47a024be0" {
+		t.Fail()
+	}
+
+	rmActivity := NotificationFeedActivity{
+		ForeignID: activity.ForeignID,
+	}
+	_ = rmActivity
+
+	err = feed.RemoveActivityByForeignID(activity)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	testCleanUp(client, nil, []*NotificationFeedActivity{activity})
+
+}
+
+func TestNotificationFeedActivities(t *testing.T) {
+
+	client, err := testSetup()
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	feed, err := client.NotificationFeed("notification", "bob")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	_, err = feed.AddActivity(&NotificationFeedActivity{
+		Verb:      "post",
+		ForeignID: "48d024fe-3752-467a-8489-23febd1dec4e",
+		Object:    FeedID("flat:eric"),
+		Actor:     FeedID("flat:john"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	activities, err := feed.Activities(&GetNotificationFeedInput{})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	for _, result := range activities.Results {
+		err = testCleanUp(client, nil, result.Activities)
+		if err != nil {
+			fmt.Println(err)
+			t.Fail()
+			return
+		}
+	}
+}
+
+func TestNotificationFeedAddActivities(t *testing.T) {
+
+	client, err := testSetup()
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	feed, err := client.NotificationFeed("notification", "bob")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	activityA, err := feed.AddActivity(&NotificationFeedActivity{
+		Verb:      "post",
+		ForeignID: "099978b6-3b72-4f5c-bc43-247ba6ae2dd9",
+		Object:    FeedID("flat:eric"),
+		Actor:     FeedID("flat:john"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	activityB, err := feed.AddActivity(&NotificationFeedActivity{
+		Verb:      "walk",
+		ForeignID: "48d024fe-3752-467a-8489-23febd1dec4e",
+		Object:    FeedID("flat:john"),
+		Actor:     FeedID("flat:eric"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	if activityA.Verb != "post" && activityA.ForeignID != "099978b6-3b72-4f5c-bc43-247ba6ae2dd9" {
+		t.Fail()
+	}
+
+	if activityB.Verb != "walk" && activityB.ForeignID != "48d024fe-3752-467a-8489-23febd1dec4e" {
+		t.Fail()
+	}
+
+	err = testCleanUp(client, nil, []*NotificationFeedActivity{activityA, activityB})
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+}
+
+func TestNotificationFeedFollow(t *testing.T) {
+
+	client, err := testSetup()
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	feedA, err := client.NotificationFeed("notification", "bob")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	feedB, err := client.FlatFeed("flat", "eric")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+
+	err = feedA.FollowFeedWithCopyLimit(feedB, 20)
+	if err != nil {
+		t.Fail()
+	}
+
+	testCleanUpFollows(client, []*FlatFeed{feedB})
+
+}
