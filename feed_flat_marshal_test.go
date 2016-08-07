81f878cdc23a3276057d8ed4f072a99f6789ba6c
romainmenke <romainmenke@gmail.com>
2016-08-07 12:01:29 +0200

    better test

--- feed_flat_marshal_test.go
+++ feed_flat_marshal_test.go
@@ -31,6 +31,7 @@ func TestActivityMetaData(t *testing.T) {
 		ForeignID: uuid.New(),
 		Actor:     FeedID("user:eric"),
 		Object:    FeedID("user:bob"),
+		Target:    FeedID("user:john"),
 		Verb:      "post",
 		TimeStamp: &now,
 		Data:      dataB,
@@ -69,14 +70,39 @@ func TestActivityMetaData(t *testing.T) {
 		t.Fail()
 	}
 
-	fmt.Println(resultActivity)
-	fmt.Println(resultActivity.ForeignID)
-	fmt.Println(string(resultActivity.Data))
-	fmt.Println(resultActivity.MetaData)
+	if resultActivity.ForeignID != activity.ForeignID {
+		t.Fail()
+	}
+	if resultActivity.Actor != activity.Actor {
+		t.Fail()
+	}
+	if resultActivity.Verb != activity.Verb {
+		t.Fail()
+	}
+	if resultActivity.Object != activity.Object {
+		t.Fail()
+	}
+	if resultActivity.Target != activity.Target {
+		t.Fail()
+	}
+	if resultActivity.TimeStamp != activity.TimeStamp {
+		t.Fail()
+	}
+	if resultActivity.MetaData["meta"] != activity.MetaData["meta"] {
+		t.Fail()
+	}
+	if string(resultActivity.Data) != string(activity.Data) {
+		t.Fail()
+	}
 
-	fmt.Println(resultActivity2)
-	fmt.Println(resultActivity2.ForeignID)
-	fmt.Println(string(resultActivity2.Data))
-	fmt.Println(resultActivity2.MetaData)
+	// fmt.Println(resultActivity)
+	// fmt.Println(resultActivity.ForeignID)
+	// fmt.Println(string(resultActivity.Data))
+	// fmt.Println(resultActivity.MetaData)
+	//
+	// fmt.Println(resultActivity2)
+	// fmt.Println(resultActivity2.ForeignID)
+	// fmt.Println(string(resultActivity2.Data))
+	// fmt.Println(resultActivity2.MetaData)
 
 }
