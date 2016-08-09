3bfd981c7f01ce0049fd1bcd16ca25b87427b769
romainmenke <romainmenke@gmail.com>
2016-08-09 11:50:16 +0200

    fix unmarshall to

--- feed_flat_io.go
+++ feed_flat_io.go
@@ -154,7 +154,12 @@ func (a *FlatFeedActivity) UnmarshalJSON(b []byte) (err error) {
 				}
 
 				for _, to := range to2D {
-					to1D = append(to1D, to...)
+					if len(to) == 2 {
+						feedStr := to[0] + " " + to[1]
+						to1D = append(to1D, feedStr)
+					} else if len(to) == 1 {
+						to1D = append(to1D, to[0])
+					}
 				}
 			}
 
